f6e51cbc22a2f8510dc32ec6ef94aae9bb36b07c
nafiur <nrsajib@gmail.com>
2023-01-21 21:57:10 +0600

    Super Admin Role Permission

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,36 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $user = new User();
+        $user->name = 'Super Admin';
+        $user->email = 'super@lms.test';
+        $user->password = bcrypt('password');
+        $user->save();
+
+        // $role = Role::create([
+        //     'name' => 'Super Admin'
+
+
+        // ]);
+
+
+
+        $role = Role::create([
+            'name' => 'Super Admin'
+
+        ]);
+        
+        $permission = Permission::create([
+            'name' => 'create-admin'
+
+        ]);
+
+        $role->givePermissionTo($permission);
+        $permission->assignRole($role);
+
+        $user->assignRole($role);
+        
+        
     }
 }
