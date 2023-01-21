61fb343b06c06b75a763cb899efaecf0a540b438
nafiur <nrsajib@gmail.com>
2023-01-21 22:01:53 +0600

    Livewire Install

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,8 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        @livewireStyles
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -20,7 +22,7 @@
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
@@ -31,5 +33,8 @@
                 {{ $slot }}
             </main>
         </div>
+
+        @livewireScripts
+
     </body>
 </html>
