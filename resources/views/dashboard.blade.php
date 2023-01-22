515ef2738f765e242a0ebf960af7ad3ab558e57e
nafiur <nrsajib@gmail.com>
2023-01-22 10:20:37 +0600

    office test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
+{{-- office test --}}
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
